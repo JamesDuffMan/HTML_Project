529a0b5714f476c07e0384f01ffc9ffa17f43164
Matt Tyrkiel <48250437+overheardMusic@users.noreply.github.com>
2019-03-20 21:30:14 +0000

    Create search_server.php

---
+++ Matt's Folder/search_server.php
@@ -0,0 +1,38 @@
+<?php
+// Server credentials so it's easy to change later
+$servername = "localhost";
+$login = "admin";
+$password = " ";
+$errors = array();
+
+// Conneting to the actual database server
+$database = new mysqli_connect($servername, $login, $password);
+
+// Checking connection to prevent errors
+if ($connect->connect_error) {
+    die("Connection failed: " . $connect->connect_error);
+} else {
+    echo "Connected successfully";
+}
+
+// Checking for Search button to be pressed
+if (isset($_POST['beard_search'])) {
+    
+    // Values from form
+    $nameOfUser = mysqli_real_escape_string($database, $_POST['name_user']);
+
+    $name_check_query = "SELECT * FROM Profile WHERE Name='$nameOfUser'";
+    $result = $database->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo $row["Name"];
+        }
+    } else {
+        echo "0 results";
+    }
+
+    
+}
+?>
