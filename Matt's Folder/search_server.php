b0bbaab5d485c405ec8ec1cd9cff170bc2b04c29
Matt Tyrkiel <48250437+overheardMusic@users.noreply.github.com>
2019-03-20 22:23:22 +0000

    Update search_server.php
    Edit: I'm stupid

--- Matt's Folder/search_server.php
+++ Matt's Folder/search_server.php
@@ -6,11 +6,11 @@ $password = " ";
 $errors = array();
 
 // Conneting to the actual database server
-$database = new mysqli_connect($servername, $login, $password);
+$database = new mysqli($servername, $login, $password);
 
 // Checking connection to prevent errors
-if ($connect->connect_error) {
-    die("Connection failed: " . $connect->connect_error);
+if ($database->connect_error) {
+    die("Connection failed: " . $database->connect_error);
 } else {
     echo "Connected successfully";
 }
@@ -18,19 +18,31 @@ if ($connect->connect_error) {
 // Checking for Search button to be pressed
 if (isset($_POST['beard_search'])) {
     
-    // Values from form
-    $nameOfUser = $_POST["name_user"];
+    if ($_POST['name_user'] == 0) {
+        // Variables needed
+        $gender = $_POST['Gender'];
+        $relationship = $_POST['Relationship'];
+        $beard = $_POST['BeardBar'];
+        $length = $_POST['Length'];
 
-    $name_check_query = "SELECT * FROM Profile WHERE Name LIKE '%$nameOfUser%'";
-    $result = $database->query($sql);
 
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo $row["Name"];
-        }
+
     } else {
-        echo "0 results";
+        // Values from form
+        $nameOfUser = $_POST["name_user"];
+
+        // Usage of LIKE allows for user-error
+        $name_check_query = "SELECT * FROM Profile WHERE Name LIKE '%$nameOfUser%'";
+        $result = $database->query($sql);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo $row["Name"];
+            }
+        } else {
+            echo "0 results";
+        }
     }
 
 
