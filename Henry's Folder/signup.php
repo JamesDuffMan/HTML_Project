5ba39f56398f8f29378e4a00b2db849fd307c8fe
henry duruji <35931313+henryhd98@users.noreply.github.com>
2019-03-20 13:56:40 +0000

    Update signup.php

--- Henry's Folder/signup.php
+++ Henry's Folder/signup.php
@@ -9,9 +9,16 @@
         }
         
         $conn = connection();
-		
+        
+        $first =$POST['username'];
+        $first =$POST['email'];
+        $first =$POST['age'];
+        $first =$POST['password'];
+        
+
+
 		$sql= "INSERT INTO users (username,email,age,password)
-		 VALUES ('username','email','age','password');";
+		 VALUES ('$username','$email','$age','$password');";
 
 		 mysqli_query($conn,$sql);
 	header("location: ../register.php?=success");
