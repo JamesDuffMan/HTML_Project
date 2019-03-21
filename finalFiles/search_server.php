c60db0cc76ba08101d81c8552ef4f114fe8fa2b1
Matt Tyrkiel <48250437+overheardMusic@users.noreply.github.com>
2019-03-21 09:31:41 +0000

    Add files via upload
    All of these should be working fine

---
+++ finalFiles/search_server.php
@@ -0,0 +1,60 @@
+<?php
+// Server credentials so it's easy to change later
+$servername = "localhost";
+$login = "admin";
+$password = " ";
+$errors = array();
+
+// Conneting to the actual database server
+$database = new mysqli($servername, $login, $password);
+
+// Checking connection to prevent errors
+if ($database->connect_error) {
+    die("Connection failed: " . $database->connect_error);
+} else {
+    echo "Connected successfully";
+}
+
+// Checking for Search button to be pressed
+if (isset($_POST['beard_search'])) {
+    
+    if ($_POST['name_user'] == 0) {
+        // Variables needed
+        $gender = $_POST['Gender'];
+        $relationship = $_POST['Relationship'];
+        $beard = $_POST['BeardBar'];
+        $length = $_POST['Length'];
+
+        $option_check_query = "SELECT * FROM Profile WHERE Gender='$gender' AND RelStatus='$relationship' AND Beard='$beard'";
+        $result = $database->query($name_check_query);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo $row["Name"];
+            }
+        } else {
+            echo "0 results";
+        }
+
+    } else {
+        // Values from form
+        $nameOfUser = $_POST["name_user"];
+
+        // Usage of LIKE allows for user-error
+        $name_check_query = "SELECT * FROM Profile WHERE Name LIKE '%$nameOfUser%'";
+        $result = $database->query($name_check_query);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo $row["Name"];
+            }
+        } else {
+            echo "0 results";
+        }
+    }
+
+
+}
+?>
---
+++ finalFiles/server.php
@@ -0,0 +1,81 @@
+<?php
+// Server credentials so it's easy to change later
+$servername = "localhost";
+$login = "admin";
+$password = " ";
+$errors = array();
+
+// Conneting to the actual database server
+$database = new mysqli($servername, $login, $password);
+
+// Checking connection to prevent errors
+if ($database->connect_error) {
+    die("Connection failed: " . $database->connect_error);
+} else {
+    echo "Connected successfully";
+}
+
+// Registering the user
+if (isset($_POST['reg_user'])) {
+
+    // All values from input on the form
+    $username = mysqli_real_escape_string($database, $_POST['username']);
+    $email = mysqli_real_escape_string($database, $_POST['email']);
+    $password = mysqli_real_escape_string($database, $_POST['password']);
+    $password1 = mysqli_real_escape_string($database, $_POST['password1']);
+
+    // Checking if username and email is unique
+    $user_check_query = "SELECT * FROM Users WHERE Username='$username' OR email='$email' LIMIT 1";
+    $result = mysqli_query($database, $user_check_query);
+    $user = mysqli_fetch_assoc($result);
+
+    if ($user) {
+        // if user does exit
+        if ($user['username'] == $username) {
+            array_push($errors, "Username already exists");
+        }
+
+        if ($user['email'] == $email) {
+            array_push($errors, "Email already exists");
+        }
+    }
+
+    // If user and email are unique, add user to database
+    if (count($errors) == 0) {
+        $password = md5($password_1); // password encryption for later
+
+        $query = "INSERT INTO Users (Username, Email, pass) VALUES('$username', '$email', '$password')";
+        mysqli_query($database, $query);
+        $_SESSION['username'] = $username;
+        $_SESSION['success'] = "You are now logged in";
+
+    }
+}
+
+// Logging in the user
+if (isset($_POST['login_user'])) {
+    $email = mysqli_real_escape_string($database, $_POST['email2']);
+    $password = mysqli_real_escape_string($database, $_POST['password']);
+  
+    if (empty($email)) {
+        array_push($errors, "Email is required");
+    }
+    if (empty($password)) {
+        array_push($errors, "Password is required");
+    }
+  
+    if (count($errors) == 0) {
+        $password = md5($password);
+        $query = "SELECT * FROM Users WHERE Email='$email' AND pass='$password'";
+        $results = mysqli_query($database, $query);
+        if (mysqli_num_rows($results) == 1) {
+          $_SESSION['email2'] = $email;
+          $_SESSION['success'] = "You are now logged in";
+          header('location: index.php');
+        } else {
+            array_push($errors, "Wrong username/password combination");
+        }
+    }
+  }
+?>
+
